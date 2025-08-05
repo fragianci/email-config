338d525645779defc60ae9ca945dae856be47d61
Francesco Giancipoli <francesco.giancipoli@synthesis3.it>
2025-08-05 14:37:15 +0200

    Add versioning scripts to package.json and display app version in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import pkg from "../package.json";
 import EmailConfigurator from "./pages/EmailConfigurator";
 
 function App() {
   return (
     <>
       <EmailConfigurator />
+      <div className="version mt-10">{pkg.version}</div>
     </>
   );
 }
